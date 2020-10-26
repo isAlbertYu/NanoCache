4815a87f4663d41e65ee318eaa50f293a834d4a7
isAlbertYu <iameinstein24@qq.com>
2020-10-26 19:25:24 +0800

    实现了lru结构

--- lru/lru.go
+++ lru/lru.go
@@ -6,10 +6,11 @@ type Cache struct {
 	maxBytes         int64                         //允许使用的最大内存
 	nBytes           int64                         //当前已使用的内存
 	doubleLinkedList *list.List                    //go标准库中的双向链表
-	cacheMap         map[string]*list.Element      //map中存储key与对应的链表节点
+	cacheMap         map[string]*list.Element      //map中存储key与对应的链表节点，节点值为entry类型
 	OnEvicted        func(key string, value Value) //当某条记录被淘汰是的回调函数
 }
 
+//双向链表中的节点值为entry类型
 type entry struct {
 	key   string
 	value Value
@@ -23,7 +24,9 @@ type Value interface {
 //当某个value被访问到时，将其在链表中的位置移动至队头
 func (c *Cache) Get(key string) (Value, bool) {
 	if element, ok := c.cacheMap[key]; ok {
-		c.doubleLinkedList.MoveToFront(element)//移动至队头
+		c.doubleLinkedList.MoveToFront(element) //移动至队头
 		kv := element.Value.(*entry)
+		return kv.value, true
 	}
+	return nil, false
 }
